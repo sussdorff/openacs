8d539ba5358969932b8282038f3964e35c5fef62
Malte Sussdorff <malte.sussdorff@cognovis.de>
2020-06-06 08:19:46 +0200

    Added secrets and config setting

--- wait-for-postgres.sh
+++ wait-for-postgres.sh
@@ -3,7 +3,7 @@
 
 set -e
 
-until PGPASSWORD=$POSTGRES_PASSWORD psql -h "postgres" -U "openacs" -c '\q'; do
+until PGPASSWORD=$(cat "/run/secrets/psql_password") psql -h "postgres" -U $(cat "/run/secrets/psql_user") -c '\q'; do
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 1
 done
